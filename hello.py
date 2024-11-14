bc085ddb48bf9541e8e417de71bba29e76901375
rodrigocedeno <rodrigo.cedeno.j@gmail.com>
2024-11-14 22:08:12 +0100

    Add a new code segment.

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
-print("Hello World!")
+import sys
 
-print("\t This is my first Git repository on GitHub.")
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+else:
+    print("You are expected to provide your name.")
+    print("You need to type:")
+    print(f"\t python {sys.argv[0]} your_name")
+    sys.exit()
+print(f"My name is {name}.")
+print()
